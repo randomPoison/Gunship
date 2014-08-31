eea4f3d2edccf0e415c12cb272c9a14bfce4bde7
David LeGare <excaliburhissheath@gmail.com>
2014-08-31 14:09:24 -0500

    Set GameObect id and index
    - The JS GameObject's now have their id and index properly set on
    construction;

--- source/Components/GameObjectComponent.cpp
+++ source/Components/GameObjectComponent.cpp
@@ -10,10 +10,17 @@ GameObjectComponent::GameObjectComponent( Ogre::SceneNode* node, const char* nam
 
 void GameObjectComponent::CreateGameObjectComponent( const v8::FunctionCallbackInfo< v8::Value >& args )
 {
+	v8::Isolate* isolate = v8::Isolate::GetCurrent();
+	v8::Isolate::Scope isolateScope( isolate );
+	v8::HandleScope handleScope( isolate );
+
 	if ( args.IsConstructCall() )
 	{
-		printf("Constructing game object!\n");
 		ComponentInfo info = Gunship::globalInstace->currentScene->AddGameObject();
+		v8::Local< v8::Object > gameObject = args.Holder();
+		gameObject->Set( v8::String::NewFromUtf8( isolate, "id" ), v8::Integer::NewFromUnsigned( isolate, info.id ) );
+		gameObject->Set( v8::String::NewFromUtf8( isolate, "index" ), v8::Integer::NewFromUnsigned( isolate, info.index ) );
+		args.GetReturnValue().Set( gameObject );
 	}
 
 }
