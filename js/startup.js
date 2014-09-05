cff3e50159ed5f8d68fef320acd9e4aeddccd5e0
David LeGare <excaliburhissheath@gmail.com>
2014-09-04 22:35:45 -0500

    Behaviors Invoked on Correct GameObject
    - Behaviors added with GameObject.AddBehavior() are now invoked on the
    object that they were added with, so if they use 'this' it will be with
    the correct object.

--- js/startup.js
+++ js/startup.js
@@ -1,10 +1,19 @@
+'use strict';
+
 Gunship.GameObject.prototype.gunship = Gunship;
 
 Gunship.behaviors = [];
 
-Gunship.AddBehavior = function( behavior )
+Gunship.keyDownEvents = {};
+Gunship.keyUpEvents = {};
+Gunship.keyPressedEvents = {};
+Gunship.keyReleasedEvents = {};
+
+Gunship.AddBehavior = function( gameObject, func )
 {
-	Gunship.behaviors.push( behavior );
+	Gunship.behaviors.push( function( delta ) {
+		func.call( gameObject, delta );
+	} );
 };
 
 Gunship.Update = function( delta )
@@ -16,4 +25,25 @@ Gunship.Update = function( delta )
 };
 
 // add convenience methods
-Gunship.GameObject.prototype.AddBehavior = Gunship.AddBehavior;
+Gunship.GameObject.prototype.AddBehavior = function( func )
+{
+	Gunship.AddBehavior( this, func );
+};
+
+Gunship.OnKeyDown = function( key, func )
+{
+	if ( !keyDownEvents[key] )
+	{
+		keyDownEvents[key] = []
+	}
+	keyDownEvents[key].push( func );
+};
+
+Gunship.OnKeyPressed = function( key, func )
+{
+	if ( !keyPressedEvents[key] )
+	{
+		keyPressedEvents[key] = [];
+	}
+	keyPressedEvents[key].push( key, func );
+};
